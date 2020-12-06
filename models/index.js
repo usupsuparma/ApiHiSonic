1128f3ab9f3c288fad1e4cd68770eee013c13b9c
usupsuparma <usup.suparma7@gmail.com>
2020-12-07 00:10:10 +0700

    fixing use env for configuration

--- models/index.js
+++ models/index.js
@@ -9,11 +9,30 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+const connection = process.env.DB_CONNECTION;
+console.log(connection);
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_DATABASE;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+//Connect to database
+ sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: connection,
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+    port: port
+});
 
 fs
   .readdirSync(__dirname)
