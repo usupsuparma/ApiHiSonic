1020ef4172b46f39f0e573f63850fbed93853be6
usupsuparma <usup.suparma@bijb.co.id>
2021-01-29 08:14:24 +0700

    fixing register

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,56 +1,64 @@
-const models = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const models = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 exports.register = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const salt = bcrypt.genSaltSync(10);
-    const passwordHash = bcrypt.hashSync(password, salt);
-    models.users.create({
-        "email": email,
-        "password": passwordHash
+  const name = req.body.name;
+  const user_name = req.body.user_name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(10);
+  const passwordHash = bcrypt.hashSync(password, salt);
+  models.users
+    .create({
+      name: name,
+      user_name: user_name,
+      email: email,
+      password: passwordHash,
     })
-        .then(result => {
-            console.log(result);
+    .then((result) => {
+      console.log(result);
 
-            res.status(201).json({
-                message: 'Register Success',
-                data: result
-            });
-        })
-        .catch(err => next(err));
+      res.status(201).json({
+        message: "Register Success",
+        data: result,
+      });
+    })
+    .catch((err) => next(err));
+};
 
-
-}
-
-exports.login = async(req, res, next) => {
-    models.users.findOne({
-        where: {
-            email: req.body.email
-        }
+exports.login = async (req, res, next) => {
+  models.users
+    .findOne({
+      where: {
+        email: req.body.email,
+      },
     })
     .then((user) => {
-        bcrypt.compare(req.body.password, user.password)
+      bcrypt
+        .compare(req.body.password, user.password)
         .then((result) => {
-            console.log(result);
+          console.log(result);
         })
-        .catch(err => {
-            console.log(err);
-        })
-        const checkLogin = bcrypt.compareSync(req.body.password, user.password);
-        if (checkLogin) {
-            const token = jwt.sign({id: user.id, role: user.role}, process.env.SECRET);
-            res.status(200).json({
-                message: 'Success Sign In',
-                token: token
-            })
-        } else {
-            res.status(401).json({
-                message: "Failed Sign in"
-            })
-        }
+        .catch((err) => {
+          console.log(err);
+        });
+      const checkLogin = bcrypt.compareSync(req.body.password, user.password);
+      if (checkLogin) {
+        const token = jwt.sign(
+          { id: user.id, role: user.role },
+          process.env.SECRET
+        );
+        res.status(200).json({
+          message: "Success Sign In",
+          token: token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Failed Sign in",
+        });
+      }
     })
-    .catch(err => {
-        next(err);
-    })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
